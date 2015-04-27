af5d048702a73c7174b77e9a7ea97c1e1559fdd0
eeallen1 <soupfinger@gmail.com>
2015-04-27 05:22:39 -0400

    sadf

--- js/search.js
+++ js/search.js
@@ -4,6 +4,7 @@ $(document).on('pageinit', '#home', function(){
        // $('#post-list').append('<li><a href="" data-id=""' + '"H4H4H4H4"</li>' + '<br>'); 
       //  $('#post-list').listview('refresh'); 
 
+   
     $('form').submit(function(e){
         $('#post-list').empty();
         e.preventDefault();
@@ -50,7 +51,7 @@ $(document).on('pageinit', '#home', function(){
         $.each(postInfo.result, function(i, row) {
             //console.log(postInfo.result); 
             if(row.ID == postInfo.id) {
-                $('#post-data').append('<img onerror=' + "this.src='http://107.170.157.210/EliSeniorProject/SeniorProject/www/img/book.png'" + ' style="width:100px;height:130px" src="'+row.Image+'">'+'<li>Title: '+row.Title+'</li>' + '<li>Edition: '+row.Edition+'</li>' + '<li>Author: '+row.Author+'</li>' + '<li>Condition: '+row.Cond+'</li>' + '<li>Contact: '+row.Contact+'</li>' + '<li>For Sale: '+row.Sell+'</li>' + '<li>Trade: '+row.Trade+'</li>' + '<li>Price: '+row.Price+'</li>' + '<li>Info: '+row.Info+'</li>');        
+                $('#post-data').append('<div><img onerror=' + "this.src='http://107.170.157.210/EliSeniorProject/SeniorProject/www/img/book.png'" + ' style="width:150px;height:150px" src="'+row.Image+'"></div>'+'<li>Title: '+row.Title+'</li>' + '<li>Edition: '+row.Edition+'</li>' + '<li>Author: '+row.Author+'</li>' + '<li>Condition: '+row.Cond+'</li>' + '<li>Contact: '+row.Contact+'</li>' + '<li>For Sale: '+row.Sell+'</li>' + '<li>Trade: '+row.Trade+'</li>' + '<li>Price: '+row.Price+'</li>' + '<li>Info: '+row.Info+'</li>');        
                 $('#post-data').listview('refresh');            
             }
         });
@@ -90,7 +91,7 @@ $(document).on('pageinit', '#home', function(){
                     console.log(JSON.stringify(row));  
                     $('#searchh3').text("Search Results");
                    // console.log(row.ID); 
-                    $('#post-list').append('<li><a href="" data-id="' + row.ID + '"><h3>' +'<img onerror=' + "this.src='http://107.170.157.210/EliSeniorProject/SeniorProject/www/img/book.png'" + ' style="width:50px;height:70px" src="'+row.Image+'"><br>'+ row.Title + '</h3><p>Author: ' +row.Author +'<br>Edition: ' + row.Edition + ' <br>Price: $' + row.Price + '</p></a></li>');
+                    $('#post-list').append('<li><a href="" data-id="' + row.ID + '"><h3>' +'<div><img onerror=' + "this.src='http://107.170.157.210/EliSeniorProject/SeniorProject/www/img/book.png'" + ' style="width:50px;height:70px" src="'+row.Image+'"></div><br>'+ row.Title + '</h3><p>Author: ' +row.Author +'<br>Edition: ' + row.Edition + ' <br>Price: $' + row.Price + '</p></a></li>');
                 });
                 $('#post-list').listview('refresh');
 
